78b09e7d9e155ad101c6856ede118d4655516713
Armando <arfega.anisgaro@gmail.com>
2020-11-05 02:06:11 +0100

    Import de rxjs/operators

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +12,14 @@ export class SpotifyService {
   }
   getNewRelease(){
     const headers = new HttpHeaders({
-      Authorization: 'Bearer BQC646Mte1vhf1Sov3SQpm_J-zR5PylHhH4aMiD3cMFwhDYA8f7I1mnakza7LOxkDFmqa_N3gkviOIXgq7o',
+      Authorization: 'Bearer BQCryVay-vAq1NIUT4Odh2_WMv9I4mRv-4klxfCvtXAKgvxFNM3r59b0ju_R0mA3KU3LqzDNmYmQB_lWdZ4',
     });
     return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20',{ headers });
   }
 
   getArtists(artist:string){
     const headers = new HttpHeaders({
-      Authorization: 'Bearer BQC646Mte1vhf1Sov3SQpm_J-zR5PylHhH4aMiD3cMFwhDYA8f7I1mnakza7LOxkDFmqa_N3gkviOIXgq7o',
+      Authorization: 'Bearer BQCryVay-vAq1NIUT4Odh2_WMv9I4mRv-4klxfCvtXAKgvxFNM3r59b0ju_R0mA3KU3LqzDNmYmQB_lWdZ4',
     });
     return this.http.get(`https://api.spotify.com/v1/search?q=${artist}&type=artist&limit=15`,{ headers });
   }
